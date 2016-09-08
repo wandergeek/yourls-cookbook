d23e64c5bc7ff445bc58f0a68da4cafdfa6d0dda
Nick Clark <n.clark@base2services.com>
2016-09-08 13:53:06 +1000

    add default gmt offset

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['yourls']['url'] = 'https://github.com/YOURLS/YOURLS/archive/1.7.1.tar.gz'
 default['yourls']['path'] = node['apache']['docroot_dir']
 default['yourls']['yours_url'] = 'defaulthostname'
+default['yourls']['gmt_offset'] = '10'
 default['yourls']['checksum'] = ''
 default['apache']['listen'] = [ '*:8880' ]
